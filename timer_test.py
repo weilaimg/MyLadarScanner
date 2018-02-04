7de77330d540f5426fe190995e3db20099da4043
weilaimg1314 <weilaimg1314@gmail.com>
2018-02-04 13:50:17 +0800

    virtual_test

--- timer_test.py
+++ timer_test.py
@@ -4,10 +4,13 @@ import time
 
 
 def fun_timer():
-    print('Hello Timer!')
     global timer
     timer = threading.Timer(0.200, fun_timer)
     timer.start()
+    print('Hello Timer!')
     
 timer = threading.Timer(0.200, fun_timer)
 timer.start()
+
+time.sleep(3) # 15秒后停止定时器
+timer.cancel()
